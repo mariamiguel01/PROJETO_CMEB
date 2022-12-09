8ca6d2b105192e1726e0423672c3eadec47f3017
ericafgomes <efgomes1206@gmail.com>
2022-12-09 12:27:30 +0000

    Update News_Exerc.java

--- app/src/main/java/org/feup/biosignals/projectbiosignals/News_Exerc.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/News_Exerc.java
@@ -1,4 +1,40 @@
 package org.feup.biosignals.projectbiosignals;
 
-public class News_Exerc {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class News_Exerc extends AppCompatActivity {
+
+    public Button button1;
+    public Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_news);
+
+        button1 = (Button) findViewById(R.id.bt1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(News_Exerc.this,show_news.class);
+                startActivity(intent);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.bt2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(News_Exerc.this,show_news.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
