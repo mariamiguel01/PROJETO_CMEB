7697b642c3441811d7d8e71c4bae3d3e6d3c4ee3
InesAMar <ines.alves.martins01@gmail.com>
2022-12-12 22:24:53 +0000

    fragment settings ok quase

--- app/src/main/java/org/feup/biosignals/projectbiosignals/ui/settings/SettingsFragment.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/ui/settings/SettingsFragment.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import org.feup.biosignals.projectbiosignals.databinding.FragmentSettingsBinding;
-import org.feup.biosignals.projectbiosignals.ui.settings.SettingsViewModel;
 
 public class SettingsFragment extends Fragment {
 
