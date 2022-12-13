41f66ed9b3f9217048ed327d813ab288b314053c
ericafgomes <efgomes1206@gmail.com>
2022-12-13 17:05:42 +0000

    Update NewsFragment.java

--- app/src/main/java/org/feup/biosignals/projectbiosignals/ui/exercises_news/NewsFragment.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/ui/exercises_news/NewsFragment.java
@@ -19,53 +19,37 @@ import org.feup.biosignals.projectbiosignals.databinding.FragmentNewsBinding;
 import org.feup.biosignals.projectbiosignals.show_news1;
 import org.feup.biosignals.projectbiosignals.show_news2;
 
-public class NewsFragment extends Fragment implements View.OnClickListener{
-
-    private FragmentNewsBinding binding;
+public class NewsFragment extends Fragment{
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NewsViewModel settingsViewModel =
                 new ViewModelProvider(this).get(NewsViewModel.class);
 
-        binding = FragmentNewsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_news, container, false);
 
         Button button1 = (Button) root.findViewById(R.id.bt1);
         Button button2 = (Button) root.findViewById(R.id.bt2);
 
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+                fr.replace(R.id.navigation_news_exercises, new show_news1());
+                fr.commit();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+                fr.replace(R.id.navigation_news_exercises, new show_news1());
+                fr.commit();
+            }
+        });
 
         return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
-    public void onClick(View view) {
-        Fragment fragment = null;
-        switch (view.getId()) {
-            case R.id.bt1:
-                fragment = new show_news1();
-                replaceFragment(fragment);
-                break;
-
-            case R.id.bt2:
-                fragment = new show_news2();
-                replaceFragment(fragment);
-                break;
-        }
-    }
-
-    public void replaceFragment(Fragment someFragment) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, someFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 
 }
