a05a3f17e9fbeb519ee3a6da020cc85fd0563f7f
ericafgomes <efgomes1206@gmail.com>
2022-12-13 15:54:05 +0000

    update fragment news

--- app/src/main/java/org/feup/biosignals/projectbiosignals/ui/exercises_news/NewsFragment.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/ui/exercises_news/NewsFragment.java
@@ -24,8 +24,6 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNews;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
