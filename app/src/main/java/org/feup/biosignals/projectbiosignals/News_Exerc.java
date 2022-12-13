9eea254d03b4ccced6656e36bc7e97b9704fb1e8
ericafgomes <efgomes1206@gmail.com>
2022-12-13 16:38:08 +0000

    News button

--- app/src/main/java/org/feup/biosignals/projectbiosignals/News_Exerc.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/News_Exerc.java
@@ -36,5 +36,7 @@ public class News_Exerc extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- app/src/main/java/org/feup/biosignals/projectbiosignals/ui/exercises_news/NewsFragment.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/ui/exercises_news/NewsFragment.java
@@ -1,18 +1,25 @@
 package org.feup.biosignals.projectbiosignals.ui.exercises_news;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.feup.biosignals.projectbiosignals.News_Exerc;
+import org.feup.biosignals.projectbiosignals.R;
 import org.feup.biosignals.projectbiosignals.databinding.FragmentNewsBinding;
+import org.feup.biosignals.projectbiosignals.show_news1;
+import org.feup.biosignals.projectbiosignals.show_news2;
 
-public class NewsFragment extends Fragment {
+public class NewsFragment extends Fragment implements View.OnClickListener{
 
     private FragmentNewsBinding binding;
 
@@ -24,6 +31,12 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        Button button1 = (Button) root.findViewById(R.id.bt1);
+        Button button2 = (Button) root.findViewById(R.id.bt2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
         return root;
     }
 
@@ -32,4 +45,27 @@ public class NewsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public void onClick(View view) {
+        Fragment fragment = null;
+        switch (view.getId()) {
+            case R.id.bt1:
+                fragment = new show_news1();
+                replaceFragment(fragment);
+                break;
+
+            case R.id.bt2:
+                fragment = new show_news2();
+                replaceFragment(fragment);
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
