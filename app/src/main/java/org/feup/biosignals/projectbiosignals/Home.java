3a17f095f61d958f31ab7850e3fda99bf1c9abe7
ericafgomes <efgomes1206@gmail.com>
2022-12-21 10:58:30 +0000

    update images/levels

--- app/src/main/java/org/feup/biosignals/projectbiosignals/Home.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/Home.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -14,9 +15,9 @@ public class Home extends AppCompatActivity {
     private ProgressBar progressBar;
     private TextView progressText;
     private TextView pointsText;
+    private ImageView imageToLoad;
     int i = 0;
     int points; //Como definir os pontos?
-    int imageToLoad =  0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +29,7 @@ public class Home extends AppCompatActivity {
         progressText = findViewById(R.id.progress_text);
 
         pointsText = findViewById(R.id.level);
+        imageToLoad = (ImageView)findViewById(R.id.image_level);
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -45,27 +47,27 @@ public class Home extends AppCompatActivity {
                 }
 
                 if (points<5) {
-                    imageToLoad = 1;
+                    imageToLoad.setImageResource(R.drawable.level1);
                     pointsText.setText("level1");
                 }
                 if (points>=5 && points<10) {
-                    imageToLoad = 2;
+                    imageToLoad.setImageResource(R.drawable.level2);
                     pointsText.setText("level2");
                 }
                 if (points>=10 && points<15) {
-                    imageToLoad = 3;
+                    imageToLoad.setImageResource(R.drawable.level3);
                     pointsText.setText("level3");
                 }
                 if (points>=15 && points<20) {
-                    imageToLoad = 4;
+                    imageToLoad.setImageResource(R.drawable.level4);
                     pointsText.setText("level4");
                 }
                 if (points>=20 && points<25) {
-                    imageToLoad = 5;
+                    imageToLoad.setImageResource(R.drawable.level5);
                     pointsText.setText("level5");
                 }
                 else {
-                    imageToLoad = 6;
+                    imageToLoad.setImageResource(R.drawable.level6);
                     pointsText.setText("level6");
                 }
             }
