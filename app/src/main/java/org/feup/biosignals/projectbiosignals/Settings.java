6c2912778f7afaa782fb6f888efd834742db0c23
ericafgomes <efgomes1206@gmail.com>
2022-12-26 13:14:58 +0000

    update vibration and sound

--- app/src/main/java/org/feup/biosignals/projectbiosignals/Settings.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/Settings.java
@@ -3,23 +3,30 @@ package org.feup.biosignals.projectbiosignals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
-    private Switch sw1,sw2,sw3;
+    private Switch sound,vibration,connection;
     Button bt_cal;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sw1 = (Switch)findViewById(R.id.switch1);
-        sw2 = (Switch)findViewById(R.id.switch2);
-        sw3 = (Switch)findViewById(R.id.switch3);
+
+        sound = (Switch)findViewById(R.id.soundSwitch);
+        vibration = (Switch)findViewById(R.id.vibrationSwitch);
+        connection = (Switch)findViewById(R.id.switch3);
         bt_cal = (Button)findViewById(R.id.calibration);
+
+        SharedPreferences getsoundsp = getSharedPreferences("soundSwitch", MODE_PRIVATE);
+        SharedPreferences getvibrationsp = getSharedPreferences("vibrationSwitch", MODE_PRIVATE);
+        sound.setChecked(getsoundsp.getBoolean("soundSwitch", false));
+        vibration.setChecked(getvibrationsp.getBoolean("vibrationSwitch", false));
 
         bt_cal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +34,34 @@ public class Settings extends AppCompatActivity {
                 // Here goes the code that saves the current data as the "zero"
             }
         });
+
+        public void SoundSwitch(View view) {
+            if(sound.isChecked()){
+                SharedPreferences.Editor soundswitchPreference = getSharedPreferences("soundSwitch", MODE_PRIVATE).edit();
+                soundswitchPreference.putBoolean("soundSwitch", true);
+                soundswitchPreference.apply();
+                sound.setChecked(true);
+            }else{
+                SharedPreferences.Editor soundswitchPreference = getSharedPreferences("soundSwitch", MODE_PRIVATE).edit();
+                soundswitchPreference.putBoolean("soundSwitch", false);
+                soundswitchPreference.apply();
+                sound.setChecked(false);
+            }
+        }
+
+
+        public void VibrationSwitch(View view) {
+            if(vibration.isChecked()){
+                SharedPreferences.Editor vibrationswitchPreference = getSharedPreferences("vibrationSwitch", MODE_PRIVATE).edit();
+                vibrationswitchPreference.putBoolean("vibrationSwitch", true);
+                vibrationswitchPreference.apply();
+                vibration.setChecked(true);
+            }else{
+                SharedPreferences.Editor vibrationswitchPreference = getSharedPreferences("vibrationSwitch", MODE_PRIVATE).edit();
+                vibrationswitchPreference.putBoolean("vibrationSwitch", false);
+                vibrationswitchPreference.apply();
+                vibration.setChecked(false);
+            }
+        }
     }
 }
