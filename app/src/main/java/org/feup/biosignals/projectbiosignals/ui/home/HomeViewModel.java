b93ce40e1db64238171d58bdf51be48143476990
mariamiguel01 <maria.migbeirao@gmail.com>
2022-12-29 14:47:08 +0000

    home

--- app/src/main/java/org/feup/biosignals/projectbiosignals/ui/home/HomeViewModel.java
+++ app/src/main/java/org/feup/biosignals/projectbiosignals/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Let's see how your posture is doing:");
     }
 
     public LiveData<String> getText() {
